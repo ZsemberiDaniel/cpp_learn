bb7b9a37503d102630f98d9f157466b9a1fe2c6b
Daniel Zsemberi <zsemberi.daniel@gmail.com>
2018-10-08 18:35:16 +0200

    masodik beadando

---
+++ beadando/masodik/cmake-build-debug/CMakeFiles/masodik.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/masodik.dir/main.cpp.obj"
+  "masodik.pdb"
+  "masodik.exe"
+  "masodik.exe.manifest"
+  "libmasodik.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/masodik.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
