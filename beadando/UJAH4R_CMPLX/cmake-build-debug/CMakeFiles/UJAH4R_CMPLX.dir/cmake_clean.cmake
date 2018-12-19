4ca9a63edadaac68f6aa06991bad493f56287f7e
Daniel Zsemberi <zsemberi.daniel@gmail.com>
2018-12-19 13:10:48 +0100

    Corrected some bugs in `beadando`

---
+++ beadando/UJAH4R_CMPLX/cmake-build-debug/CMakeFiles/UJAH4R_CMPLX.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UJAH4R_CMPLX.dir/main.cpp.obj"
+  "UJAH4R_CMPLX.pdb"
+  "UJAH4R_CMPLX.exe"
+  "UJAH4R_CMPLX.exe.manifest"
+  "libUJAH4R_CMPLX.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/UJAH4R_CMPLX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
